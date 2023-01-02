e061da1c406cfef9cfbd285b63e3aedb29e49a3c
ghiathinejad <m.ghiathinejad@gmail.com>
2023-01-02 20:31:35 +0330

    initial project

---
+++ system/Database/Traits/HasMethodCaller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace System\Database\Traits;
+
+
+trait HasMethodCaller
+{
+    private $allMethods = ['create', 'update', 'delete', 'find', 'all', 'save', 'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull', 'limit', 'orderBy', 'get', 'paginate'];
+    private $allowedMethods = ['create', 'update', 'delete', 'find', 'all', 'save', 'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull', 'limit', 'orderBy', 'get', 'paginate'];
+
+
+    public function __call($method, $args)
+    {
+        return $this->methodCaller($this, $method, $args);
+    }
+
+    public static function __callStatic($method, $args)
+    {
+        // TODO : implement here;
+    }
+
+
+    private function methodCaller($object, $method, $args)
+    {
+        // TODO: Implement
+    }
+
+
+    protected function setAllowedMethods($array)
+    {
+        $this->allowedMethods = $array;
+    }
+}
---
+++ system/Iterator/ModelIterator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace System\Iterator;
+
+class ModelIterator implements \Iterator
+{
+    private int $position;
+
+    public function __construct(private $array = [])
+    {
+        $this->position = 0;
+    }
+
+    public function current(): string|array
+    {
+        // TODO: Implement
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function next(): void
+    {
+        $this->position += 1;
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->array[$this->position]);
+    }
+}
